d5828032075db5213d68c2f80104c070655efc44
Stefan Scherer <scherer_stefan@icloud.com>
2018-01-26 10:11:06 +0100

    Let packer create the NAT network

--- test.ps1
+++ test.ps1
@@ -1,10 +1,11 @@
 $ErrorActionPreference = 'Stop'
 
-Write-Output My environment variables:
-ls env:
+# Write-Output My environment variables:
+# ls env:
 
 Write-Output Installing packer + Hyper-V
 iwr -useb https://chocolatey.org/install.ps1 | iex
+choco feature disable --name showDownloadProgress
 choco install -y packer -version 1.1.3
 Install-WindowsFeature Hyper-V-PowerShell
 
@@ -28,15 +29,15 @@ foreach ($file in $files) {
   }
 }
 
-Write-Host "Adding NAT"
-New-VMSwitch -SwitchName "packer-hyperv-iso" -SwitchType Internal
-New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter -name "vEthernet (packer-hyperv-iso)").ifIndex
-New-NetNat -Name MyNATnetwork -InternalIPInterfaceAddressPrefix 192.168.0.0/24
+#Write-Host "Adding NAT"
+#New-VMSwitch -SwitchName "packer-hyperv-iso" -SwitchType Internal
+#New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter -name "vEthernet (packer-hyperv-iso)").ifIndex
+#New-NetNat -Name MyNATnetwork -InternalIPInterfaceAddressPrefix 192.168.0.0/24
 
-Write-Host "Adding DHCP scope"
-Install-WindowsFeature DHCP -IncludeManagementTools
-Add-DhcpServerv4Scope -Name "Internal" -StartRange 192.168.0.10 -EndRange 192.168.0.250 -SubnetMask 255.255.255.0 -Description "Internal Network"
-Set-DhcpServerv4OptionValue -ScopeID 192.168.0 -DNSServer 8.8.8.8 -Router 192.168.0.1
+#Write-Host "Adding DHCP scope"
+#Install-WindowsFeature DHCP -IncludeManagementTools
+#Add-DhcpServerv4Scope -Name "Internal" -StartRange 192.168.0.10 -EndRange 192.168.0.250 -SubnetMask 255.255.255.0 -Description "Internal Network"
+#Set-DhcpServerv4OptionValue -ScopeID 192.168.0 -DNSServer 8.8.8.8 -Router 192.168.0.1
 
 Write-Host "Allow Packer http server"
 New-NetFirewallRule -DisplayName "Allow Packer" -Direction Inbound -Program "C:\ProgramData\chocolatey\lib\packer\tools\packer.exe" -RemoteAddress LocalSubnet -Action Allow
